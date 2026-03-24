695f15e141fe3ad2a708dc3c72fbf7774a54b30e
GingDev2024 <guillersantos111@gmail.com>
2026-03-24 09:57:52 +0800

    Created Database Context and UnitOfWork in Infrastructure Layer

---
+++ SnapSearch.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace SnapSearch.Infrastructure.Data
+{
+    public sealed class AppDbContext : IDisposable
+    {
+        #region Properties
+
+        public SqlConnection Connection { get; }
+
+        #endregion Properties
+
+        #region Public Constructors
+
+        public AppDbContext()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection")!;
+            Connection = new SqlConnection(connectionString);
+            Connection.Open();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public void Dispose()
+        {
+            if (Connection.State != ConnectionState.Closed)
+                Connection.Close();
+            Connection.Dispose();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ SnapSearch.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace SnapSearch.Infrastructure.Data
+{
+    public sealed class UnitOfWork : IDisposable
+    {
+        #region Fields
+
+        private readonly AppDbContext _db;
+        private SqlTransaction _transaction;
+        private bool _disposed;
+
+        #endregion Fields
+
+        #region Public Constructors
+
+        public UnitOfWork(AppDbContext db)
+        {
+            _db = db;
+            _transaction = _db.Connection.BeginTransaction();
+        }
+
+        #endregion Public Constructors
+
+        #region Properties
+
+        public SqlConnection Connection => _db.Connection;
+        public SqlTransaction Transaction => _transaction;
+
+        #endregion Properties
+
+        #region Public Methods
+
+        public void Commit()
+        {
+            _transaction?.Commit();
+            DisposeTransaction();
+        }
+
+        public void Rollback()
+        {
+            _transaction?.Rollback();
+            DisposeTransaction();
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _transaction?.Dispose();
+                _db?.Dispose();
+                _disposed = true;
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private void DisposeTransaction()
+        {
+            _transaction?.Dispose();
+            _transaction = null;
+        }
+
+        #endregion Private Methods
+    }
+}
